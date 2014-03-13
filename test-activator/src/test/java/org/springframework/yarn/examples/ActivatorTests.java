599857ae80d636a4426c69cf92aee7ba0b0feb2e
Janne Valkealahti <janne.valkealahti@gmail.com>
2014-03-13 08:04:45 +0000

    updates

--- test-activator/src/test/java/org/springframework/yarn/examples/ActivatorTests.java
+++ test-activator/src/test/java/org/springframework/yarn/examples/ActivatorTests.java
@@ -23,22 +23,17 @@ import static org.hamcrest.Matchers.greaterThan;
 
 import java.io.File;
 import java.util.List;
-import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.hadoop.yarn.api.records.YarnApplicationState;
 import org.junit.Test;
 import org.springframework.core.io.Resource;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.yarn.boot.test.junit.AbstractBootYarnClusterTests;
-import org.springframework.yarn.boot.test.junit.AbstractBootYarnClusterTests.EmptyConfig;
-import org.springframework.yarn.test.context.MiniYarnCluster;
-import org.springframework.yarn.test.context.YarnDelegatingSmartContextLoader;
+import org.springframework.yarn.test.context.MiniYarnClusterTest;
 import org.springframework.yarn.test.junit.ApplicationInfo;
 import org.springframework.yarn.test.support.ContainerLogUtils;
 
-@ContextConfiguration(loader = YarnDelegatingSmartContextLoader.class, classes = EmptyConfig.class)
-@MiniYarnCluster
+@MiniYarnClusterTest
 public class ActivatorTests extends AbstractBootYarnClusterTests {
 
 	@Test
@@ -57,23 +52,15 @@ public class ActivatorTests extends AbstractBootYarnClusterTests {
 
 		for (Resource res : resources) {
 			File file = res.getFile();
+			String content = ContainerLogUtils.getFileContent(file);
 			if (file.getName().endsWith("stdout")) {
 				// there has to be some content in stdout file
 				assertThat(file.length(), greaterThan(0l));
 				if (file.getName().equals("Container.stdout")) {
-					Scanner scanner = new Scanner(file);
-					String content = scanner.useDelimiter("\\A").next();
-					scanner.close();
 					// this is what container will log in stdout
 					assertThat(content, containsString("Hello from ActivatorPojo"));
 				}
 			} else if (file.getName().endsWith("stderr")) {
-				String content = "";
-				if (file.length() > 0) {
-					Scanner scanner = new Scanner(file);
-					content = scanner.useDelimiter("\\A").next();
-					scanner.close();
-				}
 				// can't have anything in stderr files
 				assertThat("stderr file is not empty: " + content, file.length(), is(0l));
 			}
